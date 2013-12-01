d574f585bf41412342d91818966c89836a12b7ee
Ajay Vallabh <ajay.vallabh@mail.com>
2013-11-30 23:08:13 -0500

    Output student name + final letter grade. id: teacher-letter

--- grade_calc.rb
+++ grade_calc.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'pry'
 
 # GradeReader - an object that is responsible for reading in grade data from a CSV
 class GradeReader
@@ -7,9 +8,28 @@ class GradeReader
     @file = IO.read('grades.csv')
     @file = CSV.parse("#{@file}")
   end
+  # lists students + grades
+  def import
+    @students = []
+    CSV.foreach('grades.csv') do |row|
+      name = row.shift
+      @student = { name: name, grades: row }
+      @students << @student
+    end
+    @students.each_index do |student|
+      puts "#{@students[student][:name]}'s grades: #{@students[student][:grades].map! { |x| x.to_i }}"
+    end
+  end
+  def file
+    @file
+  end
+end
 
+class Student
+  # outputs student name + average
   def grade_output
     @students = []
+    @average_grades = []
     CSV.foreach('grades.csv') do |row|
       name = row.shift
       @student = { name: name, grades: row }
@@ -18,12 +38,45 @@ class GradeReader
     @students.each_index do |student|
       @students[student][:grades].map! {|x| x.to_i}
       student_average = @students[student][:grades].inject{ |sum, x| sum + x}.to_f / @students.size
+      @average_grades << student_average
       puts "#{@students[student][:name]}'s average grade: #{student_average}"
     end
   end
-  def file
-    @file
+end
+
+
+class FinalGrade
+  # outputs student name + final letter grade
+  def grade_output
+    @students = []
+    @average_grades = []
+    @student_names = []
+    CSV.foreach('grades.csv') do |row|
+      name = row.shift
+      @student = { name: name, grades: row }
+      @students << @student
+      @student_names << @student[:name]
+    end
+    @students.each_index do |student|
+      @students[student][:grades].map! {|x| x.to_i}
+      student_average = @students[student][:grades].inject{ |sum, x| sum + x}.to_f / @students.size
+      @average_grades << student_average
+    end
+    stu_avg = @student_names.zip(@average_grades)
+    stu_avg.each do |name, avg|
+      if avg >= 90
+        puts "#{name} got Grade A"
+      elsif avg >= 80 && avg < 90
+        puts "#{name} Grade B"
+      elsif avg >= 70 && avg < 80
+        puts "#{name} Grade C"
+      elsif avg >= 60 && avg < 70
+        puts "#{name} Grade D"
+      else
+        puts "#{name} Grade F"
+      end
+    end
   end
 end
 
-part2 = GradeReader.new.grade_output
+part3 = FinalGrade.new.grade_output
