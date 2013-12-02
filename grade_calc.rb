cd084af0014b4acd42c9750292e1b08cf2bb3c95
Ajay Vallabh <ajay.vallabh@mail.com>
2013-12-02 14:17:30 -0500

    Alphabetically sort (by last name) output of final grades

--- grade_calc.rb
+++ grade_calc.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'ruby-standard-deviation'
+require 'pry'
 
 class GradeReader
   def initialize(filename)
@@ -7,14 +8,15 @@ class GradeReader
     @file = CSV.parse("#{@file}")
     @students = []
     CSV.foreach('grades.csv') do |row|
-      name = row.shift
-      @student = { name: name, grades: row }
+      first_name = row.shift
+      last_name = row.shift
+      @student = { firstname: first_name, lastname: last_name, grades: row }
       @students << @student
     end
   end
   def import_student
     @students.each_index do |student|
-      puts "#{@students[student][:name]}'s grades: #{@students[student][:grades].map! { |x| x.to_i }}"
+      puts "#{@students[student][:firstname]} #{@students[student][:lastname]}'s grades: #{@students[student][:grades].map! { |x| x.to_i }}"
     end
   end
   def file
@@ -28,15 +30,16 @@ class Student
     @students = []
     @average_grades = []
     CSV.foreach('grades.csv') do |row|
-      name = row.shift
-      @student = { name: name, grades: row }
+      first_name = row.shift
+      last_name = row.shift
+      @student = { firstname: first_name, lastname: last_name, grades: row }
       @students << @student
     end
     @students.each_index do |student|
       @students[student][:grades].map! {|x| x.to_i}
       student_average = @students[student][:grades].inject{ |sum, x| sum + x}.to_f / @students.size
       @average_grades << student_average
-      puts "#{@students[student][:name]}'s average grade: #{student_average}"
+      puts "#{@students[student][:firstname]} #{@students[student][:lastname]}'s average grade: #{student_average}"
     end
   end
 end
@@ -48,41 +51,44 @@ class FinalGrade
     @average_grades = []
     @student_names = []
     CSV.foreach('grades.csv') do |row|
-      name = row.shift
-      @student = { name: name, grades: row }
+      first_name = row.shift
+      last_name = row.shift
+      @student = { firstname: first_name, lastname: last_name, grades: row }
       @students << @student
-      @student_names << @student[:name]
+      @student_names << @student[:firstname] + " " + @student[:lastname]
     end
     @students.each_index do |student|
       @students[student][:grades].map! {|x| x.to_i}
       student_average = @students[student][:grades].inject{ |sum, x| sum + x}.to_f / @students.size
       @average_grades << student_average
     end
+    @student_names = @student_names.sort
     stu_avg = @student_names.zip(@average_grades)
+
     stu_avg.each do |name, avg|
       if avg >= 90
         puts "#{name}'s final letter grade: A"
         File.open("report_card.txt", "a") do |f|
-          f.write("#{name}'s average: #{avg} / final grade: A\n")
+          f.write("#{name}'s average: #{avg} \n#{name}'s final grade: A\n")
         end
       elsif avg >= 80 && avg < 90
         puts "#{name}'s final letter grade: B"
         File.open("report_card.txt", "a") do |f|
-          f.write("#{name}'s average: #{avg} / final grade: B\n")
+          f.write("#{name}'s average: #{avg} \n#{name}'s final grade: B\n")
         end
       elsif avg >= 70 && avg < 80
         puts "#{name}'s final letter grade: C"
         File.open("report_card.txt", "a") do |f|
-          f.write("#{name}'s average: #{avg} / final grade: C\n")
+          f.write("#{name}'s average: #{avg} \n#{name}'s final grade: C\n")
         end
       elsif avg >= 60 && avg < 70
         puts "#{name}'s final letter grade: D"
         File.open("report_card.txt", "a") do |f|
-          f.write("#{name}'s average: #{avg} / final grade: D\n")
+          f.write("#{name}'s average: #{avg} \n#{name}'s final grade: D\n")
         end
       else
         File.open("report_card.txt", "a") do |f|
-          f.write("#{name}'s average: #{avg} / final grade: F\n")
+          f.write("#{name}'s average: #{avg} \n#{name}'s final grade: F\n")
           puts "#{name}'s final letter grade: F"
         end
       end
